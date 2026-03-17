637292ae6d0150f48448c04891504affe9b13dbd
Alvin M. Antonio Jr. <alvin.antoniojr@neu.edu.ph>
2026-03-17 15:26:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1572738051-8b7da",
+  "appId": "1:162532159737:web:9eb4b10df0a27beef43228",
+  "apiKey": "AIzaSyCwQuWqEL_ilHE2ltUsZQpFRenZeAGcOsU",
+  "authDomain": "studio-1572738051-8b7da.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "162532159737"
+};
